daf9db0bd34b06273c9e0991ce9ba7e97c2a2364
baosonnguyendang <baosonnguyendang@gmail.com>
2022-07-26 23:39:26 +0700

    finish footer

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,17 +1,19 @@
-import './Header.css'
+import "./Header.css";
 export default function Header() {
-    return (
-        <div className='Header'>
-            <div className='header_1'>Hello</div>
-            <div className='header_2'>I Am <b>Thanh</b></div>
-            <div className='header_3'>
-            <i class="fa-brands fa-facebook-f"></i>
-            <i class="fa-brands fa-linkedin-in"></i>
-            <i class="fa-solid fa-play"></i>
-            <i class="fa-brands fa-spotify" ></i>
-            <i class="fa-solid fa-podcast"></i>
-            <i class="fa-brands fa-tiktok"></i>
-            </div>
-        </div>
-    )
+  return (
+    <div className="Header">
+      <div className="header_1">Hello</div>
+      <div className="header_2">
+        I Am <b>Thanh</b>
+      </div>
+      <div className="header_3">
+        <i className="fa-brands fa-facebook-f"></i>
+        <i className="fa-brands fa-linkedin-in"></i>
+        <i className="fa-solid fa-play"></i>
+        <i className="fa-brands fa-spotify"></i>
+        <i className="fa-solid fa-podcast"></i>
+        <i className="fa-brands fa-tiktok"></i>
+      </div>
+    </div>
+  );
 }
