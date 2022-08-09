6aed6a4345b4f7b771d5600a654fd6739d9baa1e
baosonnguyendang <baosonnguyendang@gmail.com>
2022-08-09 23:08:58 +0700

    about me page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TopBar from "./components/TopBar/TopBar";
 import Footer from "./components/Footer/Footer";
 
 import LandingPage from "./components/LandingPage/LandingPage";
+import AboutMePage from "./components/AboutMePage/AboutMePage";
 import BlogPage from "./components/BlogPage/BlogPage";
 import EventsPage from "./components/EventsPage/EventsPage";
 
@@ -13,6 +14,7 @@ function App() {
       <TopBar/>
       <Routes>
         <Route path="/" element={<LandingPage />}/>
+        <Route path="/aboutme" element={<AboutMePage />}/>
         <Route path="/blog" element={<BlogPage />}/>
         <Route path="/events" element={<EventsPage />}/>
       </Routes>
---
+++ src/components/AboutMePage/AboutMePage.jsx
@@ -0,0 +1,78 @@
+import "./about.css";
+
+export default function AboutMePage() {
+  return (
+    <>
+      <div className="about-page-title">About me</div>
+      <div className="about-page-content">
+        <div>
+          <div className="about-page-content-title">
+            <div></div>
+            <span>Bio</span>
+          </div>
+          <div>
+            <div></div>
+            <div>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. Lorem
+              Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="about-page-content-title">
+            <span>Employment history</span>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+            <div className="about-page-content-list">
+                <ul>
+                    <li>Maecenas in turpis at sapien tincidunt pellentesque id quis dolor.</li>
+                    <li>Donec id dolor fringilla, varius risus vestibulum, dapibus justo.</li>
+                    <li>Aliquam posuere ante ut purus lobortis, at pulvinar risus pretium.</li>
+                    <li>In tempus lectus eget ex luctus cursus.</li>
+                    <li>Mauris pharetra nunc id condimentum eleifend.</li>  
+                    <li>Ut molestie mauris ac purus interdum luctus.</li>     
+                    <li>In convallis justo at quam consequat ornare.</li>
+                    <li>Curabitur eu felis sit amet libero tincidunt condimentum.</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="about-page-content-title">
+            <div></div>
+            <span>Educational background</span>
+          </div>
+          <div>
+            <div></div>
+            <div>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. Lorem
+              Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
